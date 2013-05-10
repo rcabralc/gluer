68827d17fb17cf64ad899a1c1bd65172a5f1c725
rcabralc <rcabralc@gmail.com>
2013-05-10 19:58:38 -0300

    Remove unused method

--- lib/gluer/ordered_set.rb
+++ lib/gluer/ordered_set.rb
@@ -14,10 +14,5 @@ module Gluer
       @collection.delete_if { |existing| existing == new }
       @collection << new
     end
-
-    def -(other)
-      raise ArgumentError unless other.is_a?(OrderedSet)
-      self.class.new(@collection - other.instance_eval { @collection })
-    end
   end
 end
